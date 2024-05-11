f78555e83205707ad8e6a18107a06fd594365431
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2024-05-11 12:02:24 -0400

    basic structures for tasks and taskstore

---
+++ taskstore.go
@@ -0,0 +1,29 @@
+//first we create the Task class or structure
+
+package gorestserver
+
+import (
+	"sync"
+	"time"
+)
+
+type Task struct {
+	Id   int       `json:"id"`
+	Text string    `json:"text"`
+	Tags []string  `json:"tags"`
+	Due  time.Time `json:"due"`
+}
+
+type TaskStore struct {
+	sync.Mutex
+
+	tasks  map[int]Task
+	nextId int
+}
+
+func New() *TaskStore {
+	ts := &TaskStore{}
+	ts.tasks = make(map[int]Task)
+	ts.nextId = 0
+	return ts
+}
