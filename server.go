eca3c04f0ef07b5557d5ab39545806a40e985e4c
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2024-05-29 17:25:40 -0400

    handlers

--- server.go
+++ server.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"mime"
+	"net/http"
+	"time"
+
 	gorestserver "example.com/go-rest-server/stdlib"
 )
 
@@ -11,6 +17,55 @@ type taskServer struct {
 func NewTaskServer() *taskServer {
 	store := gorestserver.New()
 	return &taskServer{store: store}
+}
+
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+
+	log.Printf("handling task create at %s\n", req.URL.Path)
+
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	// Enforce a JSON Content-Type.
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	renderJSON(w, ResponseId{Id: id})
+
 }
 
 func main() {
